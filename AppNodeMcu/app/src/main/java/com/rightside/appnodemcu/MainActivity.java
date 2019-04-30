71093c12e698897dd2b52883c4625108a8069342
Matheus <matheusldasilva20088@gmail.com>
2019-04-30 02:53:28 -0300

    Incluido firebase + comunicação NODEMCU nome do animal display

--- AppNodeMcu/app/src/main/java/com/rightside/appnodemcu/MainActivity.java
+++ AppNodeMcu/app/src/main/java/com/rightside/appnodemcu/MainActivity.java
@@ -8,30 +8,43 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btnLampada, btnLigar;
     TextView txtResultado;
+    EditText editText;
+    private DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        databaseReference = FirebaseDatabase.getInstance().getReference("usuario");
+
         btnLampada = findViewById(R.id.btnLampada);
         btnLigar = findViewById(R.id.btnLigar);
-        txtResultado = findViewById(R.id.txtResultado);
+        editText = findViewById(R.id.editTeste);
 
 
         btnLampada.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
+               String name =  editText.getText().toString();
 
-            solicitaDados("1");
+
+                databaseReference.setValue(name);
+
+                Toast.makeText(MainActivity.this, "Salvo com sucesso", Toast.LENGTH_SHORT).show();
+
 
 
             }
